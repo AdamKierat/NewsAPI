e12c4bff58fc2c1aa9d0fdbca3320ce118a6807a
AdamKierat <adam.kierat1@gmail.com>
2021-04-16 03:15:23 +0200

    countries added

--- components/CustomSearchBar.js
+++ components/CustomSearchBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { SearchBar } from 'react-native-elements'
 import { StyleSheet, View, SafeAreaView, Text } from 'react-native'
 import { API_KEY } from '../utils/constants'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+
 
 const CustomSearchBar = ({ search, setSearch, setItemList }) => {
 
@@ -22,31 +22,31 @@ const CustomSearchBar = ({ search, setSearch, setItemList }) => {
 
     };
     return (
-        <TouchableOpacity>
-            <SearchBar
-                placeholder="Type Here..."
-                placeholderTextColor='#fff'
-                onChangeText={(text) => setSearch(text)}
-                value={search}
-                round
-                showLoading
-                setSearch={getMoviesFromApiAsync}
-                searchIcon={{ color: "#fff" }}
-                clearIcon={{ color: "#fff" }}
-                containerStyle={{
-                    backgroundColor: "#fb9327",
-                    borderBottomColor: 'transparent',
-                    borderTopColor: 'transparent'
-                }
-                }
-                inputContainerStyle={{
-                    backgroundColor: "#f0a150"
-                }}
-                inputStyle={{ color: '#fff' }}
-                onSubmitEditing={getMoviesFromApiAsync}
 
-            />
-        </TouchableOpacity>
+        <SearchBar
+            placeholder="Type Here..."
+            placeholderTextColor='#fff'
+            onChangeText={(text) => setSearch(text)}
+            value={search}
+            round
+            showLoading
+            setSearch={getMoviesFromApiAsync}
+            searchIcon={{ color: "#fff" }}
+            clearIcon={{ color: "#fff" }}
+            containerStyle={{
+                backgroundColor: "#fb9327",
+                borderBottomColor: 'transparent',
+                borderTopColor: 'transparent'
+            }
+            }
+            inputContainerStyle={{
+                backgroundColor: "#f0a150"
+            }}
+            inputStyle={{ color: '#fff' }}
+            onSubmitEditing={getMoviesFromApiAsync}
+
+        />
+
     )
 }
 
