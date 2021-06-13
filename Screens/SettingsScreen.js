9743efd7080d53847b421dff199e5823ca2d67e7
Karol Nawrot <47122661+Physalium@users.noreply.github.com>
2021-06-13 19:26:39 +0200

    darkMode redux variable

--- Screens/SettingsScreen.js
+++ Screens/SettingsScreen.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+import { switchMode } from './../redux/features/darkModeSlice'
 
 const SettingsScreen = () => {
+    const isDark = useSelector((state) => state.darkMode.isDark)
+    const dispatch = useDispatch()
     return (
         <View>
             <Text>SettingsScreen</Text>
+            <Button
+                onPress={() => dispatch(switchMode())}
+                title="mode switch"
+                color="#841584"
+                accessibilityLabel="dupa"
+            />
+            <Text>{"jest ciemno? " + isDark}</Text>
         </View>
     )
 }
