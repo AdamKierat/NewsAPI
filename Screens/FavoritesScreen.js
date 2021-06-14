bc5f6306fbef936058acf0470cd66befec4e17e6
AdamKierat <adam.kierat1@gmail.com>
2021-06-14 23:29:31 +0200

    added navigation parameter to fav screen

--- Screens/FavoritesScreen.js
+++ Screens/FavoritesScreen.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 
 
-const FavoritesScreen = () => {
+const FavoritesScreen = ({ navigation }) => {
     const isDark = useSelector((state) => state.darkMode.isDark)
     return (
         <View style={{ flex: 1, backgroundColor: isDark ? "#272121" : "#fff" }}>
