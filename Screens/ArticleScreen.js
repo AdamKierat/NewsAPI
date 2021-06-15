35888a79b8963277bbb4845918fde0c8dec0ed60
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-06-15 11:25:43 +0200

    Add article to favorites

--- Screens/ArticleScreen.js
+++ Screens/ArticleScreen.js
@@ -1,29 +1,53 @@
-import React, { useLayoutEffect, useState } from "react";
-import { StyleSheet, Text, View, Dimensions, Linking } from 'react-native'
-import { Image } from 'react-native-elements'
-import { ScrollView } from 'react-native-gesture-handler';
-import { Divider } from 'react-native-elements';
-import { format } from "date-fns";
+import React, {useEffect, useLayoutEffect, useState, useCallback} from "react";
+import {Dimensions, Linking, StyleSheet, Text, View} from 'react-native'
+import {Divider, Image} from 'react-native-elements'
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import {format} from "date-fns";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { useSelector } from 'react-redux'
-import { TouchableOpacity } from "react-native-gesture-handler";
-const ArticleScreen = ({ route, navigation }) => {
-    const { article, otherParam } = route.params;
-    const isDark = useSelector((state) => state.darkMode.isDark)
+import {useSelector} from 'react-redux'
+import firebase from "../firebase";
 
-    const [favpressed, setFavpressed] = useState(false);
+const ArticleScreen = ({route, navigation}) => {
+    console.ignoredYellowBox = ['Setting a timer'];
+    const {article, otherParam} = route.params;
+    const isDark = useSelector((state) => state.darkMode.isDark)
     const [iconColor, seticonColor] = useState("white");
 
-    const changefavoriteincon = () => {
-        if (favpressed == true) {
-            seticonColor("white");
-            setFavpressed(false);
-        } else {
-            seticonColor("red");
-            setFavpressed(true);
-        }
-    };
+    const id_ = article.url.slice(8).replace(/\//g, '_')
+    const articleDB = firebase.db.collection("articles").doc(id_);
 
+    const addFavArticle = useCallback(async () => {
+        const doc = await articleDB.get()
+
+        if(doc.exists){
+            seticonColor('white')
+            try {
+                await articleDB.delete();
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            seticonColor('red')
+            try {
+                await articleDB.set(article);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }, [])
+
+    useEffect(() => {
+        const fetchDoc = async () => {
+            const doc = await articleDB.get()
+            if(doc.exists){
+                seticonColor('red')
+            } else {
+                seticonColor('white')
+            }
+        }
+
+        fetchDoc()
+    }, [navigation])
 
     const loadInBrowser = () => {
         Linking.openURL(article.url)
@@ -41,7 +65,7 @@ const ArticleScreen = ({ route, navigation }) => {
                     }}
                 >
                     <TouchableOpacity
-                        onPress={changefavoriteincon}
+                        onPress={() => addFavArticle()}
                         activeOpacity={0.5}
                         pressMagnification={0.5}
                     >
@@ -49,7 +73,7 @@ const ArticleScreen = ({ route, navigation }) => {
                             name="cards-heart"
                             size={30}
                             color={iconColor}
-                        ></MaterialCommunityIcons>
+                        />
                     </TouchableOpacity>
                 </View>
             ),
@@ -57,23 +81,28 @@ const ArticleScreen = ({ route, navigation }) => {
     }, [navigation, iconColor]);
 
     return (
-        <View style={[styles.container, { backgroundColor: isDark ? "#272121" : "#fff" }]}>
+        <View style={[styles.container, {backgroundColor: isDark ? "#272121" : "#fff"}]}>
             <ScrollView>
                 <Image source={{
                     uri: article.urlToImage
                 }}
-                    style={styles.image} onPress={loadInBrowser} />
-                <Text style={[styles.titleText, { color: isDark ? "#fff" : "#272121" }]} >{article.title}</Text>
-                <Divider orientation="horizontal" height={2} color={'black'} margin={5} marginTop={20} marginBottom={20} />
-                <Text style={[styles.contentText, { color: isDark ? "#fff" : "black" }]}>{article.content}</Text>
-                <Text style={styles.linkText} onPress={loadInBrowser} >Read more...</Text>
-                <Divider orientation="horizontal" height={1} color={'black'} margin={5} marginTop={10} marginBottom={10} />
-                <Text style={[styles.authorText, { color: isDark ? "#fff" : "#686868" }]} >{article.author}</Text>
+                       style={styles.image} onPress={loadInBrowser}/>
+                <Text style={[styles.titleText, {color: isDark ? "#fff" : "#272121"}]}>{article.title}</Text>
+                <Divider orientation="horizontal" height={2} color={'black'} margin={5} marginTop={20}
+                         marginBottom={20}/>
+                <Text style={[styles.contentText, {color: isDark ? "#fff" : "black"}]}>{article.content}</Text>
+                <Text style={styles.linkText} onPress={loadInBrowser}>Read more...</Text>
+                <Divider orientation="horizontal" height={1} color={'black'} margin={5} marginTop={10}
+                         marginBottom={10}/>
+                <Text style={[styles.authorText, {color: isDark ? "#fff" : "#686868"}]}>{article.author}</Text>
                 <View style={styles.dateContainer}>
-                    <MaterialCommunityIcons name="clock-time-four-outline" color={isDark ? "#fff" : "#686868"} size={19} />
-                    <Text style={[styles.dateText, { color: isDark ? "#fff" : "#686868" }]}>{format(new Date(article.publishedAt), "MMM d, yyyy")}</Text>
+                    <MaterialCommunityIcons name="clock-time-four-outline" color={isDark ? "#fff" : "#686868"}
+                                            size={19}/>
+                    <Text
+                        style={[styles.dateText, {color: isDark ? "#fff" : "#686868"}]}>{format(new Date(article.publishedAt), "MMM d, yyyy")}</Text>
                 </View>
-                <Divider orientation="horizontal" height={1} color={'black'} margin={5} marginTop={10} marginBottom={10} />
+                <Divider orientation="horizontal" height={1} color={'black'} margin={5} marginTop={10}
+                         marginBottom={10}/>
             </ScrollView></View>
     )
 }
@@ -81,12 +110,12 @@ export default ArticleScreen
 const windowWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
-    container: { flex: 1, flexDirection: 'column', },
-    image: { width: windowWidth, height: 250, resizeMode: 'cover' },
-    titleText: { fontSize: 20, fontWeight: 'bold', textAlign: 'left', margin: 1, marginTop: 10 },
-    contentText: { fontSize: 19, fontWeight: 'normal', color: "black", textAlign: 'left', margin: 1 },
-    dateText: { fontSize: 19, marginLeft: 5 },
-    linkText: { fontSize: 19, fontWeight: 'bold', marginLeft: 5, color: "blue" },
-    authorText: { fontSize: 20, marginLeft: 5, alignSelf: 'center' },
-    dateContainer: { flexDirection: 'row', alignItems: 'center', justifyContent: 'center' },
+    container: {flex: 1, flexDirection: 'column',},
+    image: {width: windowWidth, height: 250, resizeMode: 'cover'},
+    titleText: {fontSize: 20, fontWeight: 'bold', textAlign: 'left', margin: 1, marginTop: 10},
+    contentText: {fontSize: 19, fontWeight: 'normal', color: "black", textAlign: 'left', margin: 1},
+    dateText: {fontSize: 19, marginLeft: 5},
+    linkText: {fontSize: 19, fontWeight: 'bold', marginLeft: 5, color: "blue"},
+    authorText: {fontSize: 20, marginLeft: 5, alignSelf: 'center'},
+    dateContainer: {flexDirection: 'row', alignItems: 'center', justifyContent: 'center'},
 })
