2b91ad1c046b710732b417aa6854d6c03e4f07b3
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-06-30 00:54:34 +0200

    Fix

--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
-import { SafeAreaView, TouchableOpacity } from 'react-native'
-import { View, StyleSheet } from 'react-native'
-import { Button, Input, Text } from 'react-native-elements'
-import { auth} from '../firebase'
+import React, {useLayoutEffect, useState} from 'react'
+import {SafeAreaView, StyleSheet, TouchableOpacity, View} from 'react-native'
+import {Button, Input, Text} from 'react-native-elements'
+import firebase, {auth} from '../firebase'
 import * as ImagePicker from 'expo-image-picker';
-import firebase from "../firebase";
 
-const RegisterScreen = ({ navigation }) => {
+const RegisterScreen = ({navigation}) => {
     const openImagePickerAsync = async () => {
         let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
@@ -21,19 +19,25 @@ const RegisterScreen = ({ navigation }) => {
             return;
         }
 
-        setSelectedImage({ localUri: pickerResult.uri });
+        setSelectedImage({localUri: pickerResult.uri});
     };
-
 
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [selectedImage, setSelectedImage] = useState('assets/765-default-avatar.png');
 
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerStyle: {backgroundColor: "#fb9327"},
+            title: 'Register',
+        })
+    }, [navigation])
+
     const register = () => {
         auth.createUserWithEmailAndPassword(email, password)
             .then(authUser => {
-                firebase.db.collection('profileImages').doc(email).set({image:selectedImage})
+                firebase.db.collection('profileImages').doc(email).set({image: selectedImage})
             })
             .then(() => {
                 console.log("Image successfully written!");
@@ -45,7 +49,7 @@ const RegisterScreen = ({ navigation }) => {
     return (
         <SafeAreaView behavior="padding" style={styles.container}>
 
-            <Text h3 style={{ marginBottom: 50 }}>Create NewsAPI account</Text>
+            <Text h3 style={{marginBottom: 50}}>Create NewsAPI account</Text>
             <View style={styles.inputContainer}>
 
                 <Input
@@ -66,15 +70,15 @@ const RegisterScreen = ({ navigation }) => {
 
 
             </View>
-            <TouchableOpacity onPress={openImagePickerAsync} style={styles.imageButton} >
-                <Text style={styles.imageButtonText} >Profile picture (optional)</Text>
+            <TouchableOpacity onPress={openImagePickerAsync} style={styles.imageButton}>
+                <Text style={styles.imageButtonText}>Profile picture (optional)</Text>
             </TouchableOpacity>
             <Button
                 containerStyle={styles.button}
-                buttonStyle={{ backgroundColor: "#fb9327" }}
+                buttonStyle={{backgroundColor: "#fb9327"}}
                 raised
                 onPress={register}
-                title="Register" />
+                title="Register"/>
 
         </SafeAreaView>
     )
